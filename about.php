2285d194b0a55f9f73595fb17b0bd40b939a0c8c
Takumi Minohara <takumi.minohara08@gmail.com>
2018-07-31 01:19:54 +0000

    Erased improper  message on about page

--- about.php
+++ about.php
@@ -8,26 +8,7 @@ Template Name: About
 
      
 	<div id="aboutpage">
-	    
-	    <p>This is the page where I suppose to talk about what this website is all about.<br>
-	    <br>
-	     <br>
-	     <br>
-	    Hi<br>
-	    <br>
-	    <br>
-	    Hello World.<br>
-
-	    <br>
-	    <br>
-	    <br>
-	    And ...<br>
-	    <br>
-	    Yeah ....<br>
-	    <br>
-	    <br>
-
-	    </p>
+	   
 	</div>
  
 <?php get_footer(); ?>
